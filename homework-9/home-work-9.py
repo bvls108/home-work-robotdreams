a8aeae525e687a7b2b4e46e4fa39ccfd12946793
bvls108 <vl.slipenko@gmail.com>
2023-03-19 14:20:51 +0200

    Перевірте будь ласка домашнє завдання 9

---
+++ homework-9/home-work-9.py
@@ -0,0 +1,11 @@
+a = int(input('Введіть яке саме число із послідовності Фібоначі треба вивести: '))
+
+def fib(n):
+    a, b = 1, 1
+    for i in range(n):
+        yield a
+        a, b = b, a + b
+
+l = list(fib(a))
+
+print(l)
